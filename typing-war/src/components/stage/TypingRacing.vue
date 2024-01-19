b038a04705fea3a1f8582a6d0383817537f04e21
nhk9710 <hknoh@wkit.co.kr>
2024-01-19 16:56:10 +0900

    [feat]countdown기능 추가

--- typing-war/src/components/stage/TypingRacing.vue
+++ typing-war/src/components/stage/TypingRacing.vue
@@ -3,30 +3,35 @@ import {ref,watch} from 'vue';
 import wordLump from '../../word_data/words.json'
 import gsap from 'gsap'
 
-//word config
-let gameState = ref(false);
-let gamePause = ref(false);
-let endMessage = ref('');
-let wordValue = ref('');
-const typing = ref('');
-const step = ref(1);
-const moveSpeed = ref(5.5);
-const npcSpeed = ref(10);
-const babyMode = ref(false);
+//word config, 전역 변수 모음 ========================
+let gameState = ref(false); //게임 패배 또는 승리 시 활성화 변수
+let gamePause = ref(false); // 게임 일시정지 활성화 변수
+let endMessage = ref(''); // 현 스테이지 종료시 메시지 변수
+let wordValue = ref(''); //입력해야 할 단어 변수
+const typing = ref(''); // 플레이어 입력 변수
+const step = ref(1); // 스테이지 단계 변수
+const moveSpeed = ref(5.5); // 플레이어 움직임 속도 변수
+const npcSpeed = ref(10); // NPC 움직임 속도 변수
+const babyMode = ref(false); // 응애모드 활성화 변수
+let count = ref(3); //게임 시작 전 카운트 다운 변수
+//변수 모음 끝 ================================
+
+//새로운 단어 선언 함수
 const newWord = () => {
   return wordValue.value = wordLump.item[Math.floor(Math.random() * 188687)]
 }
 
-// variable
+// variable, 플레이어 및 NPC 도달 좌표 변수
 let moveRange = ref(0);
 let npcRange = ref(0);
 
-//user
+//user, 플레이어 움직임 함수
 const moveBox = () => {
   moveRange.value += wordValue.value.length * moveSpeed.value;
   gsap.to(".green", {x : moveRange.value, duration:1});
 };
 
+//정답 입력시 체크 하는 코드
 const check_word = () => {
   if(wordValue.value === typing.value){
     newWord();
@@ -35,18 +40,17 @@ const check_word = () => {
   }
 };
 
-//npc player
-let moveNpc =
-  setInterval(() => {
-    npcRange.value += npcSpeed.value;
-    gsap.to(".red", {x : npcRange.value, duration:1});
-  },1000)
+//npc player, NPC움직임 관련 변수
+let moveNpc = ''
 
+
+//게임 일시정지 함수
 const pauseGame = () => {
   clearInterval(moveNpc);
   gamePause.value = true;
 }
 
+//게임 재실행 함수
 const resumeGame = () => {
   moveNpc = setInterval(() => {
     npcRange.value += npcSpeed.value;
@@ -56,6 +60,7 @@ const resumeGame = () => {
   typing.value = '';
 }
 
+//재도전,응애모드 실행 관련 함수
 const retry = (state) => {
   if(state === 'baby' && !babyMode.value){
     moveSpeed.value += 2.5;
@@ -77,11 +82,27 @@ const retry = (state) => {
       npcRange.value += npcSpeed.value;
       npcRange.value = Math.round(npcRange.value * 10)/10
       gsap.to(".red", {x : npcRange.value, duration:1});
-    },1000)
-  console.log(npcSpeed.value);
+    },1000);
+  setTimeout(startCountdown, 1000);
 }
 
+const startCountdown = () => {
+  const timer = setInterval(() => {
+    count.value--;
+    if(count.value === 0){
+      clearInterval(timer);
+      moveNpc = setInterval(() => {
+        npcRange.value += npcSpeed.value;
+        gsap.to(".red", {x : npcRange.value, duration:1});
+      },1000)
+    }
+  },1000);
+}
+
+//새로운 단어 선언 함수 실행
 newWord();
+setTimeout(startCountdown, 1000);
+//게임 승패 여부 관련 감시 코드
 watch([moveRange, npcRange], () => {
   if(moveRange.value >= 920){
     gameState.value = true;
@@ -102,15 +123,25 @@ watch([moveRange, npcRange], () => {
     <div>
       <span class="text-h5 text-white text-bold">STAGE : {{ step }}</span>
     </div>
+    <div v-if="count > 0">
+      <span class="text-h5 text-white text-bold">시작 {{ count }} 초 전...</span>
+    </div>
     <div class="box-container">
       <div class="box red"></div>
       <div class="box green"></div>
     </div>
     <div draggable="false" class="disable-dblclick">
-      <p class="text-bold text-h5 text-white disable-dblclick" >{{ wordValue }}</p>
+      <p class="text-bold text-h5 text-white disable-dblclick">{{ wordValue }}</p>
     </div>
     <div>
-      <q-input label="단어" v-model="typing" filled bg-color="white" @keyup.enter="check_word" @keyup.space="pauseGame"/>
+      <q-input
+        label="단어"
+        v-model="typing"
+        filled
+        bg-color="white"
+        :disable="count > 0"
+        @keyup.enter="check_word"
+        @keyup.space="pauseGame"/>
       <span class="text-pink-5 text-bold" v-if="babyMode">응애모드 ON!</span>
     </div>
 
@@ -135,6 +166,7 @@ watch([moveRange, npcRange], () => {
       </q-card>
     </q-dialog>
 
+    <!-- if game is pause show dialog   -->
     <q-dialog v-model="gamePause" >
       <q-card class="end-container">
         <q-card-section>
